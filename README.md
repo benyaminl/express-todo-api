9b23020c6eb5fc872bad51b346454b847eeaf04f
Benyamin Limanto <blbenyamin9@gmail.com>
2023-05-05 09:08:44 +0700

    feat(file upload & html ejs) : Introduced

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express JS Todo API
+
+This is experimental Todo API using express, with some ejs (for simple web form to see the data), with some of the API using JSON. 
+
+## Dependency
+ - ejs
+ - body-parses
+ - multer
--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var upload = multer();
+
+const multerConfig = {
+    //specify diskStorage (another option is memory)
+    storage: multer.diskStorage({
+        //specify destination
+        destination: function (req, file, next) {
+            next(null, './public/upload');
+        },
+
+        //specify the filename to be unique
+        filename: function (req, file, next) {
+            console.log(file);
+            const ext = file.mimetype.split('/')[1];
+            //set the file fieldname to a unique name containing the original name, current datetime and the extension.
+            next(null, file.fieldname + '-' + Date.now() + '.' + ext);
+        }
+    }),
+
+    // filter out and prevent non-image files.
+    fileFilter: function (req, file, next) {
+        if (!file) {
+            next();
+        }
+        // only permit zip mimetypes
+        const zip = file.mimetype.startsWith('application');
+        const image = file.mimetype.startsWith('image');
+        console.log(file.mimetype);
+        if (zip || image) {
+            console.log('zip or image uploaded');
+            next(null, true);
+        } else {
+            console.log("file not supported")
+            errorReq = true;
+            return next();
+        }
+    }
+};
+
+var upload = multer(multerConfig);
 var app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(upload.array());
+app.use(upload.any());
 app.use(express.static('public')); // Folder public static file
+//setting view engine to ejs
+app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
-    let html = "<form method='post'><input type='text' name='name'><button type='submit'>Hai</button></form>";
-
-    res.send(html);
+    res.render("index");
 });
 
 app.post("/", function (req, res) {
